c8db9eade253ed2ac4e5e89d5fbd4f35ab9ac9d8
AneeshaTomy12 <104021331+AneeshaTomy12@users.noreply.github.com>
2022-09-29 16:22:52 +0530

    Update randomnum.py

--- randomnum.py
+++ randomnum.py
@@ -1,6 +1,4 @@
 import random
-
-
 def bang_game():
     random_num = random.randrange(100, 300)
     number = str(random_num)
@@ -10,7 +8,6 @@ def bang_game():
     print("You have 3 chances")
     print("Try to guess what it is.")
     print("************************************")
-
     while (i < 3):
         print("\n#Guess:", i, end=" ")
         guess = input("\nenter a number between 100 to 300  ")
@@ -18,7 +15,6 @@ def bang_game():
             flag = 1
             break
         else:
-
             for j in range(0, 3):
                 if number[j] == guess[j]:
                     print("same", end=" ")
@@ -26,9 +22,7 @@ def bang_game():
                     print("wrong position", end=" ")
                 else:
                     print("incorrect", end=" ")
-
             pass
-
         i = i + 1
     if flag == 1:
         print("\ncorrect")
@@ -37,12 +31,8 @@ def bang_game():
         print("\nbetter luck next time")
         print(" answer is  ", number)
     play_again()
-
-
 def play_again():
     ask = input("Do you want to play again(Y/N): ")
     if ask == 'y':
         bang_game()
-
-
 bang_game()
